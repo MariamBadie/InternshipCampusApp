bccd73a8ceaa79eca746ede3a8c2aacc1ce69bd5
MariamBadie <mariam.mohamed.fawzy.44@gmail.com>
2024-08-18 14:49:25 +0300

    integrated unblock  accounts backend with the frontend

--- lib/screens/blocked_accounts_page.dart
+++ lib/screens/blocked_accounts_page.dart
@@ -1,11 +1,11 @@
 import 'package:campus_app/models/blocked_account_object.dart';
 import 'package:campus_app/widgets/blocked_accounts/blocked_accounts_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../backend/Controller/userController.dart';
-
 
 class BlockedAccounts extends StatefulWidget {
   const BlockedAccounts({super.key});
@@ -27,48 +27,69 @@ class _BlockedAccountsState extends State<BlockedAccounts> {
 
   Future<void> _fetchBlockedAccounts() async {
     String userID = 'yq2Z9NaQdPz0djpnLynN'; // Replace with actual user ID
-    List<Map<String, dynamic>> blockedAccountsData = await viewBlockedAccounts(userID);
+    List<Map<String, dynamic>> blockedAccountsData =
+        await viewBlockedAccounts(userID);
 
     setState(() {
       _availableBlockedAccounts = blockedAccountsData.map((data) {
         return BlockedAccountObject(
           blockedAccountID: data['id'],
           blockedAccountName: data['data']['name'],
-          blockedAccountProfilePic: Image.asset('assets/images/profile-pic.png', width: 200),
+          blockedAccountProfilePic:
+              Image.asset('assets/images/profile-pic.png', width: 200),
         );
       }).toList();
     });
   }
 
-void _unblockBlockedAccount(int index) async {
-  String userID = 'yq2Z9NaQdPz0djpnLynN'; // Replace with actual user ID
-  String blockedUserID = _availableBlockedAccounts[index].blockedAccountID; // Use the ID
+  void _unblockBlockedAccount(int index) async {
+    String userID = 'yq2Z9NaQdPz0djpnLynN'; // Replace with actual user ID
+    String blockedUserID =
+        _availableBlockedAccounts[index].blockedAccountID; // Use the ID
 
-  await removeFromBlockedAccounts(userID, blockedUserID);
+    await removeFromBlockedAccounts(userID, blockedUserID);
 
-  setState(() {
-    _availableBlockedAccounts.removeAt(index);
-  });
-}
-
-
-
+    setState(() {
+      _availableBlockedAccounts.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Blocked Accounts",
-          style: GoogleFonts.roboto(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
+        title: Row(
+          children: [
+            Text(
+              "Blocked Accounts",
+              style: GoogleFonts.roboto(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Text(
+              '${_availableBlockedAccounts.length}',
+              style: GoogleFonts.roboto(
+                color: const Color.fromARGB(255, 213, 12, 12),
+                fontSize: 22,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
         ),
       ),
       body: _availableBlockedAccounts.isEmpty
-          ? Center(child: Text('No blocked accounts found'))
+          ? const Center(
+              child: Text(
+                'No Blocked Accounts Found',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            )
           : BlockedAccountsList(
               accounts: _availableBlockedAccounts,
               onUnblock: _unblockBlockedAccount,
