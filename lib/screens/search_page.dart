ad0cad4145ce3d586962571dacb8510c7e43cc73
Mo7iee <135067727+Mo7iee@users.noreply.github.com>
2024-08-22 00:02:43 +0300

    edit

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -6,218 +6,6 @@ class SearchPage extends StatefulWidget {
   _SearchPageState createState() => _SearchPageState();
 }
 
-class CustomSearch extends SearchDelegate {
-  @override
-  String get searchFieldLabel => "Search Outlets";
-
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () {
-          query = "";
-        },
-        icon: Icon(Icons.close_sharp),
-      )
-    ];
-  }
-
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: Icon(Icons.arrow_back),
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    return Text("");
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return Text("");
-  }
-}
-
-class CustomSearch1 extends SearchDelegate {
-  @override
-  String get searchFieldLabel => "Search Posts";
-
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () {
-          query = "";
-        },
-        icon: Icon(Icons.close_sharp),
-      )
-    ];
-  }
-
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: Icon(Icons.arrow_back),
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    return Text("");
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return Text("");
-  }
-}
-
-class CustomSearch2 extends SearchDelegate {
-  @override
-  String get searchFieldLabel => "Search Offices";
-
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () {
-          query = "";
-        },
-        icon: Icon(Icons.close_sharp),
-      )
-    ];
-  }
-
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: Icon(Icons.arrow_back),
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    return Text("");
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return Text("");
-  }
-}
-
-class CustomSearch3 extends SearchDelegate {
-  List Profiles = [
-    "Ahmed Hany",
-    "Mohie-Eldin Mohamed",
-    "Anas Tamer",
-    "Aly Hussein",
-    "Noor Wael",
-    "Noor Ahmed",
-    "Youssef Alaa",
-    "Yassin Aly",
-    "Ahmed Adel",
-    "Ahmed Mohamed",
-    "Rana Khaled",
-    "Mariam Mohamed",
-  ];
-  List? Filter;
-  @override
-  String get searchFieldLabel => "Search Friends";
-
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () {
-          query = "";
-        },
-        icon: Icon(Icons.close_sharp),
-      )
-    ];
-  }
-
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: Icon(Icons.arrow_back),
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    return Text("");
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    if (query == "") {
-      return ListView.builder(
-        itemCount: Profiles.length,
-        itemBuilder: (context, i) {
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.person,
-                    size: 24.0,
-                  ),
-                  Container(width: 7),
-                  Text(
-                    "${Profiles[i]}",
-                    style: TextStyle(fontSize: 15),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      );
-    } else {
-      Filter = Profiles.where((element) => element.contains(query)).toList();
-      return ListView.builder(
-        itemCount: Filter!.length,
-        itemBuilder: (context, i) {
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(22.0),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.person,
-                    size: 24.0,
-                  ),
-                  Container(width: 7),
-                  Text(
-                    "${Filter![i]}",
-                    style: TextStyle(fontSize: 15),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      );
-    }
-  }
-}
-
 class _SearchPageState extends State<SearchPage> {
   final TextEditingController _searchController = TextEditingController();
   List<String> _results = [];
@@ -272,116 +60,84 @@ class _SearchPageState extends State<SearchPage> {
                 scrollDirection: Axis.horizontal,
                 padding: EdgeInsets.all(8),
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      showSearch(
-                        context: context,
-                        delegate: CustomSearch3(),
-                      );
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(right: 10),
-                      child: Column(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.grey[200],
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: Image.asset(
-                              'assets/images/friends.png',
-                              height: 45,
-                              width: 45,
-                            ),
-                            padding: EdgeInsets.all(10),
+                  Container(
+                    margin: EdgeInsets.only(right: 10),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(100),
                           ),
-                          Text("Friends"),
-                        ],
-                      ),
+                          child: Image.asset(
+                            'assets/images/friends.png',
+                            height: 45,
+                            width: 45,
+                          ),
+                          padding: EdgeInsets.all(10),
+                        ),
+                        Text("Friends"),
+                      ],
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      showSearch(
-                        context: context,
-                        delegate: CustomSearch1(),
-                      );
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(right: 10),
-                      child: Column(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.grey[200],
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: Image.asset(
-                              'assets/images/posts.png',
-                              height: 45,
-                              width: 45,
-                            ),
-                            padding: EdgeInsets.all(10),
+                  Container(
+                    margin: EdgeInsets.only(right: 10),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(100),
                           ),
-                          Text("Posts"),
-                        ],
-                      ),
+                          child: Image.asset(
+                            'assets/images/posts.png',
+                            height: 45,
+                            width: 45,
+                          ),
+                          padding: EdgeInsets.all(10),
+                        ),
+                        Text("Posts"),
+                      ],
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      showSearch(
-                        context: context,
-                        delegate: CustomSearch(),
-                      );
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(right: 10),
-                      child: Column(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.grey[200],
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: Image.asset(
-                              'assets/images/outlets.png',
-                              height: 45,
-                              width: 45,
-                            ),
-                            padding: EdgeInsets.all(10),
+                  Container(
+                    margin: EdgeInsets.only(right: 10),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(100),
                           ),
-                          Text("Outlets"),
-                        ],
-                      ),
+                          child: Image.asset(
+                            'assets/images/outlets.png',
+                            height: 45,
+                            width: 45,
+                          ),
+                          padding: EdgeInsets.all(10),
+                        ),
+                        Text("Outlets"),
+                      ],
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      showSearch(
-                        context: context,
-                        delegate: CustomSearch2(),
-                      );
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(right: 10),
-                      child: Column(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.grey[200],
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: Image.asset(
-                              'assets/images/officess.png',
-                              height: 45,
-                              width: 45,
-                            ),
-                            padding: EdgeInsets.all(10),
+                  Container(
+                    margin: EdgeInsets.only(right: 10),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            borderRadius: BorderRadius.circular(100),
                           ),
-                          Text("Offices"),
-                        ],
-                      ),
+                          child: Image.asset(
+                            'assets/images/officess.png',
+                            height: 45,
+                            width: 45,
+                          ),
+                          padding: EdgeInsets.all(10),
+                        ),
+                        Text("Offices"),
+                      ],
                     ),
                   ),
                   Container(
@@ -424,20 +180,6 @@ class _SearchPageState extends State<SearchPage> {
                 "Clear all",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
-            ),
-          ),
-
-          Expanded(
-            child: ListView.builder(
-              itemCount: _results.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_results[index]),
-                  onTap: () {
-                    // Handle item tap
-                  },
-                );
-              },
             ),
           ),
         ],
